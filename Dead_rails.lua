23c62ec9f9d25785a637a9ae91d6ec9c1c2150e4
kdal <134873992+Kamil2808@users.noreply.github.com>
2025-05-29 20:43:24 +0300

    Update Dead_rails.lua

--- Dead_rails.lua
+++ Dead_rails.lua
@@ -77,6 +77,7 @@ if game.PlaceId == 70876832253163 then
     end
 
 elseif game.PlaceId == 116495829188952 then
+    wait(3)
     if tonumber(game:GetService("Players").LocalPlayer.PlayerGui.BondDisplay.BondInfo.BondCount.Text) > getgenv().bonds then
         local OSTime = os.time()
 		local Time = os.date('!*t', OSTime)
