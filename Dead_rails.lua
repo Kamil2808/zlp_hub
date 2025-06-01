15014cc7a3d6fab93514e9f58fa3b8e249edc8d5
kdal <134873992+Kamil2808@users.noreply.github.com>
2025-06-01 21:31:37 +0300

    Update Dead_rails.lua

--- Dead_rails.lua
+++ Dead_rails.lua
@@ -70,7 +70,7 @@ if game.PlaceId == 70876832253163 then
         getgenv().disable_auto_execute = true
         getgenv().auto_bond = true
         print("ye1")
-        loadstring(game:HttpGet("https://get.nathub.xyz/loader"))();
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/ArdyBotzz/NatHub/refs/heads/master/NatHub.lua"))();
         local x = true
         local v = true
         local f = true
