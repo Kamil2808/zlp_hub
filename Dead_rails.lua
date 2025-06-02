c6513bfdf1bc10bc7bcfad29e5f184d503e1eba2
kdal <134873992+Kamil2808@users.noreply.github.com>
2025-06-03 02:39:06 +0300

    Update Dead_rails.lua

--- Dead_rails.lua
+++ Dead_rails.lua
@@ -118,7 +118,7 @@ if game.PlaceId == 70876832253163 then
                     v = false
                 end
 	    	end
-            if ((DateTime.now().UnixTimestamp - dt) > 40) and (CardScreen.Background.MainFrame.Holder:GetChildren()[4]:GetChildren()[3].Text == "0") then
+            if ((DateTime.now().UnixTimestamp - dt) > 80) and (CardScreen.Background.MainFrame.Holder:GetChildren()[4]:GetChildren()[3].Text == "0") then
                 local ts = game:GetService("TeleportService")
                 local p = game:GetService("Players").LocalPlayer
                 ts:Teleport(116495829188952, p)
